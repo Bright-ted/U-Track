2e66dde74a91d3dde0f1c3c252be57ff2b75d5f1
Bright-Ted <ekpebright57@gmail.com>
2026-02-01 17:05:37 +0000

    Force Python 3.11 and let pip choose Pillow version

--- app.py
+++ app.py
@@ -3085,6 +3085,4 @@ $$ LANGUAGE plpgsql;
 """
 
 if __name__ == "__main__":
-    # For production on Render
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000, debug=False)
